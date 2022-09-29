1a02f3b7377336507c5319da392558e08784532a
vishalshinde541 <vishalshinde541@gmail.com>
2022-09-29 13:09:13 +0530

    Initial commit of line comparison computatio

---
+++ README.md
@@ -0,0 +1,1 @@
+# Line-comparison-problem
---
+++ src/com/Bridgelabz/LineComparisonComputaion/LineComparisonComputation.java
@@ -0,0 +1,10 @@
+package com.Bridgelabz.LineComparisonComputaion;
+
+public class LineComparisonComputation {
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Line Comparison computation");
+		
+	}
+
+}

8ff311fe60106268d0df55605eca22a0079e13cf
vishalshinde541 <vishalshinde541@gmail.com>
2022-10-13 22:45:33 +0530

    cheking equality of two lines

--- src/com/Bridgelabz/LineComparisonComputaion/LineComparisonComputation.java
+++ src/com/Bridgelabz/LineComparisonComputaion/LineComparisonComputation.java
@@ -4,9 +4,20 @@ import java.util.Scanner;
 
 public class LineComparisonComputation {
 	public static void main(String[] args) {
-		
 		System.out.println("Welcome to Line Comparison computation");
-			
+		
+		float lengthOfLine_1 = calculatelengthOfLine_1();
+		float lengthOfLine_2 = calculatelengthOfLine_2();
+		
+			if (lengthOfLine_1 == lengthOfLine_2) {
+				System.out.println("Both lines arw equal");
+			}else {
+				System.out.println("Lines are not equal");
+			}
+	}
+	static float calculatelengthOfLine_1() {
+		System.out.println("Enter co-ordinate values for Line_1");
+		
 		Scanner src = new Scanner(System.in);
 		System.out.print("Enter value of x1 coordinate for point P1 :");
 		int x1 = src.nextInt();
@@ -17,10 +28,26 @@ public class LineComparisonComputation {
 		System.out.print("Enter value of y2 coordinate for point P2 :");
 		int y2 = src.nextInt();
 		
-		double length = Math.sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+		float length = (float) Math.sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+		return length;
 		
-		System.out.println("Length of line : " + length + " mm");
+	}
+	
+	static float calculatelengthOfLine_2() {
+		System.out.println("Enter co-ordinate values for Line_2");
 		
+		Scanner src = new Scanner(System.in);
+		System.out.print("Enter value of x1 coordinate for point P1 :");
+		int x1 = src.nextInt();
+		System.out.print("Enter value of y1 coordinate for point P1 :");
+		int y1 = src.nextInt();
+		System.out.print("Enter value of x2 coordinate for point P2 :");
+		int x2 = src.nextInt();
+		System.out.print("Enter value of y2 coordinate for point P2 :");
+		int y2 = src.nextInt();
+		
+		float length = (float) Math.sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+		return length;
 		
 	}
 

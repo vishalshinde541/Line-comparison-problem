e0e86990ef2221c7ea42d2490017bd08b3564dff
vishalshinde541 <vishalshinde541@gmail.com>
2022-10-13 22:59:54 +0530

    Comparing two lines

--- src/com/Bridgelabz/LineComparisonComputaion/LineComparisonComputation.java
+++ src/com/Bridgelabz/LineComparisonComputaion/LineComparisonComputation.java
@@ -9,15 +9,20 @@ public class LineComparisonComputation {
 		float lengthOfLine_1 = calculatelengthOfLine_1();
 		float lengthOfLine_2 = calculatelengthOfLine_2();
 		
-		System.out.println(lengthOfLine_1);
-		System.out.println(lengthOfLine_2);
+		System.out.println("length of 1st line : " + lengthOfLine_1);
+		System.out.println("length of 2nd line : " + lengthOfLine_2);
 		
-			if (lengthOfLine_1 == lengthOfLine_2) {
-				System.out.println("Both lines are equal");
-			}else {
-				System.out.println("Lines are not equal");
-			}
-	}
+		 Integer l1=new Integer((int) lengthOfLine_1);
+	     Integer l2=new Integer((int) lengthOfLine_2);
+
+	        if (l1.compareTo(l2) > 0) {
+	            System.out.println("line 1 is greater than line 2");
+	        }else if (l1.compareTo(l2) < 0) {
+	            System.out.println("line 1 is smaller than line 2");
+	        }else
+	            System.out.println("both Lines are equal");
+	    }
+			
 	static float calculatelengthOfLine_1() {
 		System.out.println("Enter co-ordinate values for Line_1");
 		

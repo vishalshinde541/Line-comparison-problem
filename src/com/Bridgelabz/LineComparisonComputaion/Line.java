0c56b227bea73dc848e4e5cf52019eda13d5ba61
vishalshinde541 <vishalshinde541@gmail.com>
2022-10-13 23:45:15 +0530

    Line Comparison using OOPS

---
+++ src/com/Bridgelabz/LineComparisonComputaion/Line.java
@@ -0,0 +1,17 @@
+package com.Bridgelabz.LineComparisonComputaion;
+
+public class Line {
+	Point p1;
+    Point p2;
+    int length;
+
+    public Line(Point p1, Point p2) {
+    	 this.p1 = p1;
+         this.p2 = p2;
+	}
+
+	float getLength(){
+		return (float) Math.sqrt( Math.pow((p2.x- p1.x),2) + Math.pow((p2.y- p1.y),2) );
+    }
+
+}
---
+++ src/com/Bridgelabz/LineComparisonComputaion/Point.java
@@ -0,0 +1,14 @@
+package com.Bridgelabz.LineComparisonComputaion;
+
+public class Point {
+	int x;
+	int y;
+	
+	public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+	
+	
+	}
+
+}
